b47e77282b13d32cd36672f7671f4574bcb6e40c
Nirma Nethmini <nirmanethmini@gmail.com>
2025-07-28 20:47:27 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -2,3 +2,7 @@ import re
 
 def check_password_strength(password):
     length = len(password)
+    has_upper = bool(re.search(r'[A-Z]', password))
+    has_lower = bool(re.search(r'[a-z]', password))
+    has_digit = bool(re.search(r'\d', password))
+    has_symbol = bool(re.search(r'[\W_]', password))
